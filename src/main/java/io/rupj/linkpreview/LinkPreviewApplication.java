7642c549c58edddf2557491b8a7e97a12ce9975c
Rupin Jairaj <rupinjairaj@gmail.com>
2020-05-19 23:25:29 +0530

    initial commit

---
+++ src/main/java/io/rupj/linkpreview/LinkPreviewApplication.java
@@ -0,0 +1,13 @@
+package io.rupj.linkpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkPreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkPreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rupj/linkpreview/LinkPreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rupj.linkpreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkPreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
